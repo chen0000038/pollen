547abdcea2c9bbe4565276966432def06c44e30c
Haoyuan Sun <hsun0044@student.monash.edu>
2025-04-09 21:50:07 +1000

    Changing the background

--- src/Views/PollenTracker.vue
+++ src/Views/PollenTracker.vue
@@ -430,19 +430,19 @@ export default {
           vizElement.style.minWidth = '420px'
           vizElement.style.maxWidth = '650px'
           vizElement.style.width = '100%'
-          vizElement.style.minHeight = '587px'
-          vizElement.style.maxHeight = '887px'
-          vizElement.style.height = divElement.offsetWidth * 0.75 + 'px'
+          vizElement.style.minHeight = '687px'
+          vizElement.style.maxHeight = '987px'
+          vizElement.style.height = divElement.offsetWidth * 0.85 + 'px'
         } else if (divElement.offsetWidth > 500) {
           vizElement.style.minWidth = '420px'
           vizElement.style.maxWidth = '650px'
           vizElement.style.width = '100%'
-          vizElement.style.minHeight = '587px'
-          vizElement.style.maxHeight = '887px'
-          vizElement.style.height = divElement.offsetWidth * 0.75 + 'px'
+          vizElement.style.minHeight = '687px'
+          vizElement.style.maxHeight = '987px'
+          vizElement.style.height = divElement.offsetWidth * 0.85 + 'px'
         } else {
           vizElement.style.width = '100%'
-          vizElement.style.height = '727px'
+          vizElement.style.height = '827px'
         }
         const scriptElement = document.createElement('script')
         scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'
