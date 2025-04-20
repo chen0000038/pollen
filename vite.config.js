317b9094adfe9474f4294d4a98cae2d651ca73e2
chen0038 <chen0038@student.monash.edu>
2025-04-20 16:00:14 +1000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-
 export default defineConfig({
   plugins: [vue()],
-  base: './', 
-  publicDir: 'public'
+  base: process.env.VITE_BASE || '/'
 })
 
