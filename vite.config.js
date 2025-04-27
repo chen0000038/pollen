053b74f97aacf152bdac06f1848e2d5a57a410a7
chen0038 <chen0038@student.monash.edu>
2025-04-28 02:29:37 +1000

    fix: use history mode & set base to /iteration2/

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  base: './', 
+
+  base: mode === 'production' ? '/iteration2/' : '/',
   publicDir: 'public'
-})
+}))
 
